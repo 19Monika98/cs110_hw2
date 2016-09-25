8a67d19d936f9ba16b375c9c28e66fa370848c75
19Monika98 <monika_martirosyan@edu.aua.am>
2016-09-25 13:38:57 +0400

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,14 @@
+<script>
+const factorial = function(n) {
+    if(n === 0 || n === 1) 
+	{
+        return 1;
+    } 
+	{
+        return n * factorial(n - 1);
+    }
+};
+ 
+console.log(factorial(5));
+
+</script>  
